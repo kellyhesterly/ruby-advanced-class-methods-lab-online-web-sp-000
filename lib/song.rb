60ada1f327c6ace7854a215a9c2f03a000a0af77
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-10 16:40:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,9 @@ end
 
 def self.new_from_filename(filename)
    song = self.new
-   song.name = filename.chomp(".mp3").split(" - ")
+   song.name = split_filename[1]
+   song.artist_name = split_filename[0]
+   split_filename = filename.chomp(".mp3").split(" - ")
    binding.pry
 end
 

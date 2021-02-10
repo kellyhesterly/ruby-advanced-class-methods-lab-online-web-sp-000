9551dfd680dd1cc93a3bc08d22b0e85a8131c30e
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-10 15:53:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -18,38 +16,34 @@ class Song
       song
     end
 
-    def self.new_by_name(name)
-      song = self.new
-      song.name = name
-      song
-    end
-
-    def self.create_by_name(name)
-      song = self.new
-      song.name = name
-      @@all << song
-      song
-    end
-
-    def self.find_by_name(name)
-      @@all.find {|song| song.name == name}
-    end
-
-    def self.find_or_create_by_name(name)
-     return find_by_name(name) || create_by_name(name)
-    end
-
-    def self.alphabetical
-      self.all.sort_by{|song| song.name}
-    end
-
-    def self.new_from_filename(name)
-      new_name = name.split(" ")
-      artist = new_name[0]
-      name.slice! ".mp3"
-      binding.pry
-      song = self.new
-      song.name = name
-    end
-
+def self.new_by_name(name)
+  song = self.new
+  song.name = name
+  song
 end
+
+def self.create_by_name(name)
+  song = self.new
+  song.name = name
+  @@all << song
+  song
+end
+
+def self.find_by_name(name)
+  @@all.find {|x| x.name == name}
+end
+
+def self.find_or_create_by_name(name)
+  self.find_by_name(name) || self.create_by_name(name)
+end
+
+def self.alphabetical()
+  @@all.sort_by {|song| song.name}
+end
+
+def self.new_from_filename(name)
+   song = self.new
+end
+
+
+
